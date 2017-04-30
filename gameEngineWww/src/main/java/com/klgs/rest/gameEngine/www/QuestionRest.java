bbe4d940e05c088c455f04063e76e7f1f178922f
Pulkit Gupta <pulgupta@gmail.com>
2017-04-30 22:51:03 +0530

    updated CROs

--- gameEngineWww/src/main/java/com/klgs/rest/gameEngine/www/QuestionRest.java
+++ gameEngineWww/src/main/java/com/klgs/rest/gameEngine/www/QuestionRest.java
@@ -1,11 +1,16 @@
 package com.klgs.rest.gameEngine.www;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.klgs.rest.gameEngine.www.model.Question;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //When we are caling a REST API from another application
+//running on a particular server and port we will have to specify that we want to allow 
+//access to that another server application. For this we have added this CROS else we will get
+//403 in our angular application
 @RequestMapping(value="/question")
 public class QuestionRest {
 
