b0fe7ea38a993dcbfd6dbd0a4a7a56ab9b4d82d4
Pulkit Gupta <pulgupta@gmail.com>
2017-05-01 09:45:00 +0530

    resolved bugs

--- gameEngineWww/src/main/java/com/klgs/rest/gameEngine/www/QuestionRest.java
+++ gameEngineWww/src/main/java/com/klgs/rest/gameEngine/www/QuestionRest.java
@@ -1,6 +1,7 @@
 package com.klgs.rest.gameEngine.www;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.klgs.rest.gameEngine.www.model.Question;
 public class QuestionRest {
 
 	@RequestMapping(value="", method=RequestMethod.POST)
-	public Question saveQuestion(Question question) {
+	public Question saveQuestion(@RequestBody Question question) {
 		System.out.println("This is inside the post method of Question" + question);
 		return question;
 	}
