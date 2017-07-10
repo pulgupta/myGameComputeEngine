c3c6d72f9d6b8a714d0a8df0e0d97fae923adab2
Pulkit Gupta <pulgupta@gmail.com>
2017-07-10 21:56:55 +0530

    added api gateway

---
+++ gameApiGateway/src/main/java/com/klgs/rest/gameEngine/gateway/gameApiGateway/GameApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.klgs.rest.gameEngine.gateway.gameApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApiGatewayApplication.class, args);
+	}
+}
